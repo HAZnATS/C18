dbc6e744bf2e7c4123d56b3547fd908e4f74ab76
HAZnATS <87596379+HAZnATS@users.noreply.github.com>
2021-11-08 14:44:46 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var student1;
+var student2
+function setup() {
+  createCanvas(400,400);
+student1= new Student("joe",12);
+student1.display();
+student2= new Student("mohds",13);
+student2.display();
+}
+
+function draw() 
+{
+  background(30);
+  
+}
---
+++ student.js
@@ -0,0 +1,17 @@
+
+class Student
+{
+   constructor(name,age){
+       this.name=name;
+       this.age=age;
+
+   }
+   display(){
+       console.log("student name"+this.name);
+       console.log("student age"+this.age);
+   }
+}
+
+
+
+
